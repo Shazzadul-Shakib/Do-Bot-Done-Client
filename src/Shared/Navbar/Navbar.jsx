8b776e4759704dfc144fa38e49c7794f0913b1c4
Shazzadul-Shakib <shakib1186@gmail.com>
2023-09-03 23:51:19 +0600

    React router setup and layout created

---
+++ src/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import logo from '../../assets/Do-Bot-Done-logo.svg'
+
+const Navbar = () => {
+    return (
+        <div>
+            <img src={logo} alt="" />
+        </div>
+    );
+};
+
+export default Navbar;
