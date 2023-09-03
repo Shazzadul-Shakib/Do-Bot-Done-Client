d4caad497c525675aef754205ddb4c8b7cdf14f7
Shazzadul-Shakib <shakib1186@gmail.com>
2023-09-04 01:30:34 +0600

    Navbar for large screen added

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,10 +1,36 @@
 import logo from '../../assets/Do-Bot-Done-logo.svg'
+import { AiOutlineSearch } from "react-icons/ai";
+import { FiSun, FiBell, FiPlus } from "react-icons/fi";
 
 const Navbar = () => {
     return (
-        <div>
-            <img src={logo} alt="" />
+      <div>
+        {/* Upper navbar section */}
+        <div className="bg-bg flex justify-between items-center mx-8">
+          {/* logo */}
+          <div>
+            <img className="h-14" src={logo} alt="" />
+          </div>
+          {/* Search bar */}
+          <div className=" relative max-w-md">
+            <div className=" absolute inset-y-0 pl-1 flex items-center pointer-events-none">
+              <AiOutlineSearch className=" text-lg" />
+            </div>
+            <input
+              className="block w-full py-1 pl-10 pr-4 rounded-lg border border-secondary focus:outline focus:outline-primary "
+              type="text"
+            />
+          </div>
+          {/* Icons for theme,notifications */}
+          <div className=" flex">
+            <FiSun className="text-xl mx-4"/>
+            <FiBell className="text-xl mx-4"/>
+            <FiPlus className="text-xl mx-4"/>
+          </div>
         </div>
+        {/* Lower navbar section contains navlinks */}
+        <div></div>
+      </div>
     );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: "#352F44",
+      secondary: "#5C5470",
+      accent: "#B9B4C7",
+      bg: "#FAF0E6",
+    },
   },
   plugins: [],
 };
